5d4a591a6393ccdc4fbeb469b5afa498084d5948
Benja-san <beugnet.freelance@gmail.com>
2025-03-25 16:18:48 +0100

    refactor burger menu functionality to streamline event listener implementation

--- js/index.js
+++ js/index.js
@@ -6,6 +6,10 @@ const restaurants = RESTAURANTS
 const main = document.getElementById("root")
 const list = document.createElement("ul")
 
+function onBurgerButtonClick() {
+  burgerMenu.classList.toggle("hidden")
+}
+
 restaurants.map((restaurant) => {
   const card = document.createElement("li")
   card.innerHTML = `
@@ -13,14 +17,22 @@ restaurants.map((restaurant) => {
     <img src="${restaurant.picture}" alt="${restaurant.name}" />
     <address> ${restaurant.address} </address>
     `
+  // Or you can decompose operation
+  // const name = document.createElement("h2")
+  // const picture = document.createElement("img")
+  // const address = document.createElement("address")
+  // name.textContent = restaurant.name
+  // address.textContent = restaurant.address
+  // picture.src = restaurant.picture
+  // picture.alt = restaurant.name
+  // card.appendChild(name)
+  // card.appendChild(picture)
+  // card.appendChild(address)
+
   list.appendChild(card)
 })
 
 main.appendChild(list)
-
-function onBurgerButtonClick() {
-  burgerMenu.classList.toggle("hidden")
-}
 
 burgerButton.addEventListener("click", onBurgerButtonClick)
 closeMenu.addEventListener("click", onBurgerButtonClick)
